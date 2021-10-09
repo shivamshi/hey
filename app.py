a3433bfe5a877e56755ab06739dab321c9b5f522
shivam <python7370@gmail.com>
2021-10-09 09:57:49 +0530

    bew

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, Response
 from camera import VideoCamera
 app=Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
